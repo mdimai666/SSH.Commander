abed534b512c9bc5ddd448402d1c03a8a26a9bd7
mdimai666 <mdimai666@mail.ru>
2024-02-02 13:24:01 +0900

    upd project file add tests

---
+++ src/SshCommander.Tests/Attributes/ManualFactAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Zyfra.Eom.Integration.Tests.Attributes;
+
+public class ManualFactAttribute : FactAttribute
+{
+    public ManualFactAttribute()
+    {
+        if (!Debugger.IsAttached)
+        {
+            Skip = "Only running in interactive mode.";
+        }
+    }
+}
+
---
+++ src/SshCommander.Tests/TestSshConnect.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Zyfra.Eom.Integration.Tests.Attributes;
+
+namespace SshCommander.Tests;
+
+public class TestSshConnect
+{
+    [ManualFact]
+    public async Task EchoText()
+    {
+        // Arrange
+        var client = new SshCommanderClient("sber1");
+        var expectString = "123";
+
+        // Act
+        var response = await client.RequestAsync("echo 123");
+
+        // Assert
+        response.Trim().Should().Be(expectString);
+    }
+
+}
